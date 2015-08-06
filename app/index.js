551718d8e166b6159ac67a55d1bc098b40997d97
davy engone <davy@philos.io>
2015-08-06 20:29:36 +0200

    added home module

--- app/index.js
+++ app/index.js
@@ -1,1 +1,14 @@
-console.log('the app is running');
+import angular from 'angular';
+import kayakHeader from './modules/common/directives/header.js';
+//
+
+var {bootstrap} = angular;
+
+
+angular.module('kayak', [])
+.directive('kyHeader', kayakHeader);
+
+
+
+bootstrap(document.body, ['kayak']);
+
--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,66 @@
+var webpack = require("webpack"),
+    BrowserSyncPlugin = require('browser-sync-webpack-plugin');
+
+var browerSync = new BrowserSyncPlugin({
+        host: 'localhost',
+        port: 9000,
+        server: { baseDir: ['dist'] }
+    });
+
+var jqueryPlugin = new webpack.ProvidePlugin({
+        $: "jquery",    
+        jQuery: "jquery",
+        "window.jQuery": "jquery"
+    });
+
 module.exports = {
-	entry: '.app/index.js',
-	output: {
+	entry: './app/index.js',
+	output:{
 		filename: 'build.js',
-		path: 'dist'
-	}
+        path: 'dist'
+	},
+
+ 	module: {
+        loaders: [
+            { 
+            	test: /\.js$/, 
+            	exclude: /node_modules/,
+            	loader: "babel" 
+            },
+
+            { 
+                test: /\.css$/, 
+                loader: "style-loader!css-loader" 
+            },
+
+            { 
+                test: /\.html$/, 
+                loader: "raw" 
+            },
+
+            { 
+                test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/, 
+                loader: "url-loader?limit=10000&minetype=application/font-woff" 
+            },
+            
+            { 
+                test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/, 
+                loader: "file-loader" 
+            },
+
+            { 
+                test: "\.jpg$", 
+                loader: "file" 
+            },
+            { 
+                test: "\.png$", 
+                loader: "url" 
+            }]
+    },
+
+    plugins: [ jqueryPlugin, browerSync ],
+
+    devServer: {
+        contentBase: "./dist"
+    }
 };
